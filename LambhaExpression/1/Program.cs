126ef2a44e45b23b32993f83af47d1328705483b
Kezal chavda <Kezal Chavda>
2022-02-22 23:02:24 +0530

    first commit

---
+++ LambhaExpression/1/Program.cs
@@ -0,0 +1,71 @@
+﻿public delegate string MyDelegate(); //declaring a delegate
+
+class Account
+{
+
+    static int _accountNo;
+
+    static int _balance;
+
+    public Account(int accountNo, int balance)
+    {
+        _accountNo = accountNo;
+        _balance = balance;
+    }
+    public int AccountNo
+    {
+        get => _accountNo;
+    }
+
+    public int Balance
+    {
+        get => _balance;
+    }
+    public static string Message()
+    {
+        if (_balance < 0)
+        {
+            return "you are overdrawn!\n";
+        }
+        else if (_balance < 50)
+        {
+            return "your account is very low!\n";
+        }
+        else if (_balance < 100)
+        {
+            return "watch your spending carefully!\n";
+        }
+        else
+        {
+            return "you have over $100 in your account!\n";
+        }
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyDelegate message = Account.Message;
+
+        Account a1 = new Account(11011, 450);
+
+        Console.WriteLine("\nAccount no.      Balance                   Message\n");
+
+        Console.WriteLine("  " + a1.AccountNo + "           " + a1.Balance + "            " + message());
+
+        Account a2 = new Account(11012, 75);
+
+        Console.WriteLine("  " + a2.AccountNo + "            " + a2.Balance + "            " + message());
+
+        Account a3 = new Account(11013, -12);
+
+        Console.WriteLine("  " + a3.AccountNo + "           " + a3.Balance + "            " + message());
+
+        Account a4 = new Account(11014, 35);
+
+        Console.WriteLine("  " + a4.AccountNo + "            " + a4.Balance + "             " + message());
+
+    }
+}
---
+++ SelectionSort/Program.cs
@@ -0,0 +1,109 @@
+﻿
+   
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp18jan2022
+{
+    class SelectionSorter
+    {
+        static public void Sort<T>(IList<T> sortArray, Func<T, T, bool> comparision)
+        {
+
+            for (int i = 0; i < sortArray.Count - 1; i++)
+            {
+                int key = i;
+                for (int j = i + 1; j < sortArray.Count; j++)
+                {
+                    // find minimum element from the array
+                    if (comparision(sortArray[j], sortArray[key]))
+                    {
+                        key = j;
+                    }
+                }
+                // place the minimum element in the correct position
+                T temp = sortArray[i];
+                sortArray[i] = sortArray[key];
+                sortArray[key] = temp;
+            }
+
+        }
+
+    }
+    public enum Designations
+    {
+        CEO = 5,
+        CFO = 4,
+        Assistent = 2,
+        SeniorDeveloper= 1,
+        Accountant = 3
+    }
+
+    class Employee
+    {
+        int empid;
+        public float salary;
+        public string name;
+        public Designations designation;
+
+        public Employee(int EmpId, float Salary, string Name, Designations design)
+        {
+            this.empid = EmpId;
+            this.salary = Salary;
+            this.name = Name;
+            this.designation = design;
+
+        }
+
+        internal static bool CompareSalary(Employee e1, Employee e2)
+        {
+            return e1.salary < e2.salary;
+        }
+
+        internal static bool CompareDesignations(Employee e1, Employee e2)
+        {
+            return e1.designation > e2.designation;
+        }
+    }
+
+
+    public class TestGenericMethods
+    {
+        public static void Main(string[] args)
+        {
+            List<Employee> emplist = new List<Employee>(30);
+
+            emplist.Add(new Employee(101, 100000, "Meera", Designations.CFO));
+
+            emplist.Add(new Employee(102, 50000, "Peter", Designations.Accountant));
+
+            emplist.Add(new Employee(103, 120000, "Emily", Designations.CEO));
+
+            emplist.Add(new Employee(104, 45000, "Jack", Designations.Assistent));
+
+            emplist.Add(new Employee(105, 75000, "Marco", Designations.SeniorDeveloper));
+
+
+            Console.WriteLine("\n");
+
+            // based on designation
+            SelectionSorter.Sort<Employee>(emplist, Employee.CompareDesignations);
+            Console.WriteLine("Sorting on basis of Designation (Highest to Lowest) : \n");
+            foreach (Employee e1 in emplist)
+            {
+                Console.WriteLine(e1.name + "   " + e1.designation);
+            }
+
+            Console.WriteLine("\n");
+
+            // based on salary
+            SelectionSorter.Sort<Employee>(emplist, Employee.CompareSalary);
+            Console.WriteLine("Sorting on basis of Salary (Lowest to Highest) : \n");
+            foreach (Employee e1 in emplist)
+            {
+                Console.WriteLine(e1.name + "   " + e1.salary);
+            }
+        }
+    }
+}
